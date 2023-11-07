334b7c5ca0e0082004b920857cc991653d8d27a7
nmemarcoding <nmemarcoding@gmail.com>
2023-11-06 21:01:36 -0800

    Express server with MongoDB, implementing a simple user schema and routes.

---
+++ api/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 1024
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    rule:{
+        enum: ['doctor', 'patient'],
+        type: String,
+        default: 'patient'
+    },
+    
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+    if (!this.membershipNumber) {
+        const lastUser = await mongoose.model('User').findOne().sort('-membershipNumber');  // Find the user with the highest membershipNumber
+        if (!lastUser) {
+            // No users in the database, start from 10000000
+            this.membershipNumber = 12345678;
+        } else {
+            this.membershipNumber = lastUser.membershipNumber + 1;
+        }
+    }
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ api/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+const app = express();
+
+// Middleware to enable CORS
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Connect to MongoDB using the environment variable
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connection Successful!"))
+    .catch((err) => {
+        console.log("DB Connection Error: ", err);
+    });
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+
+// Use the PORT environment variable, or default to 3002 if it's not set
+const port = process.env.PORT || 3002;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
