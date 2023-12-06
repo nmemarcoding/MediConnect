d8b93ab4f5ed697712728b45ffc042eafa0fbe38
nmemarcoding <nmemarcoding@gmail.com>
2023-12-05 18:54:07 -0800

    Add GET route for retrieving patient profile by ID

--- api/routes/patientProfile.js
+++ api/routes/patientProfile.js
@@ -47,4 +47,22 @@ router.post('/create', async(req, res) => {
     }
 });
 
+// Route to get patient profile by patient ID
+router.get('/:patientId', async(req, res) => {
+    try {
+        // validate patient ID
+        if (!mongoose.isValidObjectId(req.params.patientId)) {
+            return res.status(400).json("Invalid patient ID");
+        }
+        const patientProfile = await PatientProfile.findOne({ user: req.params.patientId });
+        if (!patientProfile) {
+            return res.status(404).json("Patient profile not found");
+        }
+        res.status(200).json(patientProfile);
+    } catch (err) {
+        res.status(500).json({ message: "An error occurred", error: err.message });
+        console.log(err);
+    }
+});
+
 module.exports = router;
