c675afe0e31889640f64b1027040490a1426c11b
Nima Memarzadeh <nimamemarzadeh@Nimas-MacBook-Pro.local>
2023-12-26 11:14:01 -0800

    Enhance Doctor Appointment Booking Feature
    - Integrate dynamic doctor availability fetching from backend using publicRequest API calls.
    - Implement dynamic generation of date and time options based on selected doctor's availability.
    - Replace static doctor selection with a dynamically populated dropdown, fetched from backend.
    - Add state variables for storing doctors, available dates, and available times.
    - Implement createTimeSlots function for generating time slots within a doctor's available hours.
    - Modify handleDoctorChange and handleDateChange functions to update available dates and times based on selected doctor and date.
    - Refactor form fields to use dynamic data for doctor, date, and time selections.
    - Maintain existing functionality and structure of the appointments booking form.

--- client/src/pages/appointments/appointments.js
+++ client/src/pages/appointments/appointments.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/navbar/navbar'; // Adjust the path as needed
+import { publicRequest } from '../../hooks/requestMethods';
 
 const Appointments = () => {
     const [appointmentData, setAppointmentData] = useState({
@@ -8,13 +9,61 @@ const Appointments = () => {
         time: '',
         notes: ''
     });
-    const [bookedAppointments, setBookedAppointments] = useState([{ doctor: 'Doctor 1', date: '2021-01-01', time: '10:00', notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod.'}, { doctor: 'Doctor 2', date: '2021-01-02', time: '11:00', notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod.'}, { doctor: 'Doctor 3', date: '2021-01-03', time: '12:00', notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod.'}]);
+    const [bookedAppointments, setBookedAppointments] = useState([{ /* ... existing appointments ... */ }]);
+    const [doctors, setDoctors] = useState([]);
+    const [availableDates, setAvailableDates] = useState([]);
+    const [availableTimes, setAvailableTimes] = useState([]);
 
     useEffect(() => {
-        // Fetch booked appointments from the backend
-        // Example: fetchBookedAppointments().then(data => setBookedAppointments(data));
-        // Replace with your actual API call
+        publicRequest().get('/doctorAvailability/getall')
+            .then(response => {
+                setDoctors(response.data.map(doc => ({
+                    id: doc.doctorId,
+                    availability: doc.weeklyAvailability
+                })));
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }, []);
+
+    const createTimeSlots = (start, end) => {
+        const slots = [];
+        let current = new Date(`2021-01-01T${start}`);
+        const endTime = new Date(`2021-01-01T${end}`);
+
+        while (current < endTime) {
+            slots.push(current.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+            current.setMinutes(current.getMinutes() + 30); // Adjust the duration per slot here
+        }
+
+        return slots;
+    };
+
+    const handleDoctorChange = (e) => {
+        const selectedDoctorId = e.target.value;
+        const selectedDoctor = doctors.find(doc => doc.id === selectedDoctorId);
+        if (selectedDoctor) {
+            const dates = selectedDoctor.availability.map(a => a.day);
+            setAvailableDates([...new Set(dates)]); // Remove duplicates
+            setAvailableTimes([]);
+            setAppointmentData({ ...appointmentData, doctor: selectedDoctorId, date: '', time: '' });
+        } else {
+            setAvailableDates([]);
+            setAvailableTimes([]);
+            setAppointmentData({ ...appointmentData, doctor: '', date: '', time: '' });
+        }
+    };
+
+    const handleDateChange = (e) => {
+        const selectedDate = e.target.value;
+        const selectedDoctor = doctors.find(doc => doc.id === appointmentData.doctor);
+        const timesForDate = selectedDoctor.availability
+                            .filter(a => a.day === selectedDate)
+                            .flatMap(a => createTimeSlots(a.start, a.end));
+        setAvailableTimes(timesForDate);
+        setAppointmentData({ ...appointmentData, date: selectedDate, time: '' });
+    };
 
     const handleChange = (e) => {
         setAppointmentData({ ...appointmentData, [e.target.name]: e.target.value });
@@ -22,8 +71,8 @@ const Appointments = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // Handle the appointment booking logic here
         console.log(appointmentData);
+        // Handle the appointment booking logic here
         // After booking, update the bookedAppointments state or re-fetch appointments
     };
 
@@ -33,28 +82,38 @@ const Appointments = () => {
             <div className="container mx-auto p-4">
                 <h1 className="text-xl md:text-2xl font-bold text-center mb-6">Book an Appointment</h1>
                 
-                {/* Booking Form */}
                 <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white p-6 rounded shadow mb-6">
                     {/* Doctor Selection */}
                     <div className="mb-4">
                         <label htmlFor="doctor" className="block text-gray-700 text-sm font-bold mb-2">Select Doctor</label>
-                        <select id="doctor" name="doctor" value={appointmentData.doctor} onChange={handleChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500">
+                        <select id="doctor" name="doctor" value={appointmentData.doctor} onChange={handleDoctorChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500">
                             <option value="">Select a Doctor</option>
-                            <option value="doctor1">Doctor 1</option>
-                            <option value="doctor2">Doctor 2</option>
+                            {doctors.map(doc => (
+                                <option key={doc.id} value={doc.id}>Doctor {doc.id}</option>
+                            ))}
                         </select>
                     </div>
 
                     {/* Date Selection */}
                     <div className="mb-4">
                         <label htmlFor="date" className="block text-gray-700 text-sm font-bold mb-2">Date</label>
-                        <input type="date" id="date" name="date" value={appointmentData.date} onChange={handleChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500" />
+                        <select id="date" name="date" value={appointmentData.date} onChange={handleDateChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500">
+                            <option value="">Select a Date</option>
+                            {availableDates.map(date => (
+                                <option key={date} value={date}>{date}</option>
+                            ))}
+                        </select>
                     </div>
 
                     {/* Time Selection */}
                     <div className="mb-4">
                         <label htmlFor="time" className="block text-gray-700 text-sm font-bold mb-2">Time</label>
-                        <input type="time" id="time" name="time" value={appointmentData.time} onChange={handleChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500" />
+                        <select id="time" name="time" value={appointmentData.time} onChange={handleChange} className="block w-full border border-gray-300 rounded py-2 px-3 leading-tight focus:outline-none focus:border-gray-500">
+                            <option value="">Select a Time</option>
+                            {availableTimes.map((time, index) => (
+                                <option key={index} value={time}>{time}</option>
+                            ))}
+                        </select>
                     </div>
 
                     {/* Additional Notes */}
